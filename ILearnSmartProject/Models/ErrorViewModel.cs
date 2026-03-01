92df15ccb3421bd7acb76c72ec75fa8708f49e1a
Devanshu <devanshudave022@gmail.com>
2026-03-01 09:40:37 -0500

    Add project files.

---
+++ ILearnSmartProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ILearnSmartProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
